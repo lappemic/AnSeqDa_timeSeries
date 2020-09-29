3213e6be8d0f67a7b11e759931f275aca34a60e6
lappemic <61876623+lappemic@users.noreply.github.com>
2020-09-29 17:43:09 +0200

    Initial setup

---
+++ AnSeqDa_project_ts_lappm1_v1.R
@@ -0,0 +1,27 @@
+############################### Project on time series ###############################
+
+#############################################
+# Projectgroup
+# Janine Maron      maronjan@students.zhaw.ch
+# Michael Lappert   lappm1@bfh.ch
+#############################################
+
+# 1.Task
+## Select a monthly or quarterly time series from the M3 competition. Analyse the time series,
+## choose meaningful indicators and fit a simple and a exponential smoothing model interpret
+## the corresponding residuals and performance. Do the same with the ets and auto.arima.
+## In the end document your findings.
+
+# 2. Selecting the time series
+
+# 3. Exploratory Analysis
+
+# 4. Indicators
+
+# 5. Simple Model
+
+# 6. Exponential Smoothing
+
+# 7. ETS and AUTO.ARIMA
+
+# 8. Conclusion
